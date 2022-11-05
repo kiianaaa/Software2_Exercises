f034c42a89bfe98d4f7373f3d501ed8ed0bfc7c0
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-06 00:00:15 +0200

    final version

--- Exercise-11.py
+++ Exercise-11.py
@@ -12,7 +12,8 @@ class Book(Publication):
         super().__init__(name)
 
     def print_info(self):
-        print(f"Name is: {self.name}, Author is: {self.author}, Page count is: {self.page}")
+        print(
+            f"Name is: {self.name}, Author is: {self.author}, Page count is: {self.page}")
 
 
 class Magazine(Publication):
@@ -29,3 +30,45 @@ magazine = Magazine("Donald Duck", "Aki Hyppa")
 
 book.print_info()
 magazine.print_info()
+
+
+# 2
+
+class Car:
+
+    def __init__(self, registration_number, maximum_speed, current_speed):
+        self.registration_number = registration_number
+        self.maximum_speed = maximum_speed
+        self.current_speed = current_speed
+        self.travelled_distance = 0
+
+    def accerelate(self, speed_change):
+        new_speed = car.current_speed + speed_change
+        if new_speed < 0:
+            self.current_speed = 0
+        elif self.maximum_speed > new_speed and new_speed > 0:
+            self.current_speed += speed_change
+
+    def brake(self):
+        if self.current_speed > 200:
+            self.current_speed -= 200
+        print("Emergency Brake!")
+
+    def drive(self, hour):
+        self.travelled_distance += self.current_speed * hour
+
+
+class ElectricCar(Car):
+    def __init__(self, reg_number, max_speed, battery_cap):
+        self.battery_cap = battery_cap
+        super().__init__(reg_number, max_speed)
+
+
+class GasolineCar(Car):
+    def __init__(self, reg_number, max_speed, tank):
+        self.tank = tank
+        super().__init__(reg_number, max_speed)
+
+
+eleccar = ElectricCar("ABC-15", 180, 52.5)
+gascar = GasolineCar("ACD-123", 165, 32.3)
