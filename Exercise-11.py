fb19ba54eaf9f7cfed6aa239db0457ad9751cc6b
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-05 23:53:25 +0200

    draft

--- Exercise-11.py
+++ Exercise-11.py
@@ -6,7 +6,26 @@ class Publication:
 
 
 class Book(Publication):
-    def __init__(self, name, author, page_count):
+    def __init__(self, name, author, page):
         self.author = author
-        self.page_count = page_count
+        self.page = page
         super().__init__(name)
+
+    def print_info(self):
+        print(f"Name is: {self.name}, Author is: {self.author}, Page count is: {self.page}")
+
+
+class Magazine(Publication):
+    def __init__(self, name, chief):
+        self.chief = chief
+        super().__init__(name)
+
+    def print_info(self):
+        print(f"Name is: {self.name}, chief is: {self.chief})")
+
+
+book = Book("Campartment No. 6", "Rosa Liksom", 192)
+magazine = Magazine("Donald Duck", "Aki Hyppa")
+
+book.print_info()
+magazine.print_info()
