7f3b1fbfbafa641634affa8661774ce857b4abe5
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-05 23:43:20 +0200

    draft

--- Exercise-11.py
+++ Exercise-11.py
@@ -3,3 +3,10 @@
 class Publication:
     def __init__(self, name):
         self.name = name
+
+
+class Book(Publication):
+    def __init__(self, name, author, page_count):
+        self.author = author
+        self.page_count = page_count
+        super().__init__(name)
