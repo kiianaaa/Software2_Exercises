0683b82c1b88cb64ba99e1cf174c64e8ad7b6b19
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-19 15:22:29 +0200

    Exercise 12

--- Exercise-12.py
+++ Exercise-12.py
@@ -1,4 +1,8 @@
 import json
 import requests
 
-# 1 random Chuck Norris joks 
+# 1 random Chuck Norris joks
+response = "https://api.chucknorris.io/jokes/random"
+joke = requests.get(response)
+json = joke.json()
+print(json["value"])
