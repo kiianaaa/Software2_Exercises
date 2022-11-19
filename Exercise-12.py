ee1c098bdd4779731d0ab799f4e8971265331119
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-19 15:37:34 +0200

    Exercise 12

--- Exercise-12.py
+++ Exercise-12.py
@@ -2,7 +2,25 @@ import json
 import requests
 
 # 1 random Chuck Norris joks
+
 response = "https://api.chucknorris.io/jokes/random"
 joke = requests.get(response)
 json = joke.json()
 print(json["value"])
+
+
+# 2 weather API
+
+city = input("Please enter the name of the city you want to check: ")
+key = ("I didn't register :) ")
+
+request = "https://api.openweathermap.org/data/2.5/weather?q=" + \
+    city + "&appid=" + key + "&units=metric"
+
+try:
+    response = requests.get(request)
+    if response.status_code == 200:
+        json_response = response.json()
+
+    except requests.exceptions.RequestException as e:
+        print("We countered an error. Try again.")
