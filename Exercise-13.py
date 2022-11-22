b33bea552ef0fcf2e395d1e6a480c574df1f2549
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-22 16:16:41 +0200

    Done

--- Exercise-13.py
+++ Exercise-13.py
@@ -1,5 +1,16 @@
 from flask import Flask, request, Response
 import json
+import mysql.connector
+
+
+connection = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='root',
+         autocommit=True
+)
 
 # 1 is the number prime
 
@@ -26,3 +37,33 @@ def prime_number(number):
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=5000)
 
+
+
+# 2 gives name and location 
+
+app = Flask(__name__)
+
+
+@app.route('/airport/<ICAO>')
+def airport(ICAO):
+    sql = "SELECT name, municipality FROM airport"
+    sql += " WHERE ident='" + ICAO + "'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount > 0:
+        for row in result:
+            name = row[0]
+            location = row[1]
+
+    response = {
+        "Airport" : name,
+        "Location" : location,
+        "ICAO" : ICAO
+    }
+    return response
+
+
+
+if __name__ = '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
